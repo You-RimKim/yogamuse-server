8f3f8704a3bf57fe7f1c8845beb4cbfda37d36b3
You-Rim Kim <yourimykim@gmail.com>
2023-09-29 16:33:47 +0200

    setup categories

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  res.json("this is the homepage");
+});
+
+router.get("/about", (req, res, next) => {
+  res.json("this is the about page");
+});
+
+router.get("/categories", (req, res, next) => {
+  res.json("here are all the categories");
 });
 
 module.exports = router;
